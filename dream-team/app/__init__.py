c26cac8557a0201459c832c175b23119414ba9f3
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2017-12-12 17:25:57 -0500

    Admin Blueprint

--- dream-team/app/__init__.py
+++ dream-team/app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 from config import app_config
 
@@ -22,6 +23,7 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config = True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    Bootstrap(app)
     db.init_app(app)
 
     login_manager.init_app(app)
---
+++ dream-team/app/auth/views.py
@@ -0,0 +1,76 @@
+# app/auth/views.py
+""" Routes for auth """
+
+from flask import flash, redirect, render_template, url_for
+from flask_login import login_required, login_user, logout_user
+
+from app.auth.forms import LoginForm, RegistrationFrom
+from . import auth
+from .. import db
+from .. models import Employee
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    """
+    Handle requests to /register route
+    Add an employee to DB through Regitration form
+    """
+
+    form = RegistrationFrom()
+    if form.validate_on_submit():
+        employee = Employee(email=form.email.data,
+                            username=form.username.data,
+                            first_name=form.first_name.data,
+                            last_name=form.last_name.data,
+                            password=form.password.data)
+
+        #add employee to db
+        db.session.add(employee)
+        db.session.commit()
+        flash('Regitration Successful, you can now login')
+
+        # Redirect to login page
+        return redirect(url_for('auth.login'))
+
+    # load registration template
+    return render_template('auth/register.html', form=form, title='Register')
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    Handle login requests
+    Log employee in through login form
+    """
+    form = LoginForm()
+    if form.validate_on_submit():
+        
+        # check wether employee exists in db and password matches
+        employee = Employee.query.filter_by(email=form.email.data).first()
+        if employee is not None and employee.verify_password(form.password.data):
+            # log employee in
+            login_user(employee)
+
+            # redirect to dashboard after login
+            return redirect(url_for('home.dashboard'))
+
+            # When login details are incorrect
+        else:
+            flash('Invalid user login or password')
+
+    #Render Login Template
+    return render_template('auth/login.html', form=form, title='Login')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    """ 
+    Handle Logout requests
+    """
+
+    logout_user()
+    flash('Logout Successful')
+
+    # redirect to login page
+    return redirect(url_for('auth.login'))
+    
+    
