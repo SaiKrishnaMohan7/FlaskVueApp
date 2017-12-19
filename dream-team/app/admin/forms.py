0c50cbdca029fc19cfc07640f2afe5c476d5a223
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2017-12-18 22:14:19 -0500

    Added Departments

--- dream-team/app/admin/forms.py
+++ dream-team/app/admin/forms.py
@@ -1,1 +1,14 @@
 # app/admin/forms.py
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class DepartmentForm(FlaskForm):
+    """ 
+    Add/Edit Departments Admin user
+    """
+
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
