e749bf5448594bc76e00449b271486ec669699ed
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2017-12-27 15:15:18 -0500

    Added Employee Assignment Feature

--- dream-team/app/admin/views.py
+++ dream-team/app/admin/views.py
@@ -4,9 +4,9 @@ from flask import abort, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from . import admin
-from forms import DepartmentForm, RoleForm
+from forms import DepartmentForm, RoleForm, EmployeeAssignForm
 from .. import db
-from ..models import Department, Role
+from ..models import Department, Role, Employee
 
 def check_admin():
     """
@@ -192,3 +192,47 @@ def delete_role(id):
     return render_template(title = 'Delete Role')
 
     
+# Employee Views
+
+@admin.route('/employees')
+@login_required
+def list_employees():
+    """ 
+    List all employees
+    """
+
+    check_admin()
+    employees = Employee.query.all()
+
+    return render_template('admin/employees/employees.html',
+                            employees = employees, title = 'Employees')
+
+@admin.route('/employees/assign/<int:id>', methods = ['GET', 'POST'])
+@login_required
+def assign_employee(id):
+    """ 
+    Admin Assign Employee role and department
+    """
+
+    check_admin()
+
+    employee = Employee.query.get_or_404(id)
+
+    # admin shouldn't be assigned a role
+    if employee.is_admin:
+        abort(403)
+    
+    form = EmployeeAssignForm(obj = employee)
+    if form.validate_on_submit():
+        employee.department = form.department.data
+        employee.role = form.role.data
+
+        db.session.add(employee)
+        db.session.commit()
+        flash('Employee assigned role and deparment')
+
+        return redirect(url_for('admin.list_employees'))
+
+    return render_template('admin/employees/employee.html',
+                            employee = employee, form = form,
+                            title = 'Assign Employees')
